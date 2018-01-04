5e5daee75ae27da12fd3873fadf7be9bf0d75d95
Dexter Jones <William.Jones@IgnitionOne.com>
2018-01-03 22:44:43 -0500

    Initial edits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,30 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { DataProvider } from '../providers/data';
+//Pages
+import { LoginPage } from '../pages/login/login';
+import * as firebase from 'firebase';
 
-import { HomePage } from '../pages/home/home';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, dataProvider: DataProvider, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      platform.pause.subscribe(()=>{
+        if(firebase.auth().currentUser)
+          firebase.database().ref('accounts/'+firebase.auth().currentUser.uid).update({'online': false});
+      });
+      platform.resume.subscribe(()=>{
+        if(firebase.auth().currentUser && localStorage.getItem('showOnline') == 'true')
+          firebase.database().ref('accounts/'+firebase.auth().currentUser.uid).update({'online': true});
+      })
     });
   }
 }
-
